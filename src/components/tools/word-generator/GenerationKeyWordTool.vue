0ca05afcb14fe88c4361d1348280e4dbe1eac71a
mateus-lopes <mateusalbano22@gmail.com>
2023-11-25 07:29:49 -0300

    final fantasy

--- src/components/tools/word-generator/GenerationKeyWordTool.vue
+++ src/components/tools/word-generator/GenerationKeyWordTool.vue
@@ -20,14 +20,12 @@ async function GeneratorKey() {
 }
 </script>
 <template>
-<<<<<<< HEAD
   <section class="pt-32 pb-32">
-=======
->>>>>>> 4adbb0eaf28834dbc741de7f1b7c8dc916d8fb34
   <article>
     <ToolTitle text="Geração de palavras-chave " />
     <div class="flex-center gap-10">
       <div class="gradient-border w-4/6 h-[36vh]">
+        Texto
         <p class="w-full p-2">
           {{ text }}
         </p>
