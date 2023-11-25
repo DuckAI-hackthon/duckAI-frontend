023f763af506a6e02c1dfdd0a87d8df8b991ac30
luiznunes06 <luiz.nunes180106@gmail.com>
2023-11-24 22:57:34 -0300

    feat - criacao da pagina de resumo de texto

---
+++ src/components/tools/GenerationTextToolComponent.vue
@@ -0,0 +1,23 @@
+<script>
+import ToolTitleComponent from './ToolTitleComponent.vue'
+import GenTextContainerComponent from './GenTextContainerComponent.vue'
+
+export default {
+    components: {
+        GenTextContainerComponent,
+        ToolTitleComponent
+    },
+}
+
+
+</script>
+
+<template>
+    <main>
+        <ToolTitleComponent text="Geração de resumo "/>
+        <GenTextContainerComponent/>
+    </main>
+        
+
+
+</template>
