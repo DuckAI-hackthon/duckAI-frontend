c3dd05fb32d0f731798f4b89f80c4d4331bda6e2
Wecreds <Wecredsbr@gmail.com>
2023-11-25 11:17:45 -0300

    página de about e icone e nome do site

--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -2,8 +2,6 @@
 import TitleSpecial from '@/components/TitleSpecial.vue'
 import axios from 'axios';
 import { onMounted, ref } from 'vue'
-import CirclesComponent from '../components/login/CirclesComponent.vue'
-
 
 const devsData = ref([])
 
@@ -23,21 +21,26 @@ onMounted(() => {
 })
 </script>
 <template>
+    <Router-link to="/">
+        <div class="cursor-pointer text-white text-2xl m-4 absolute top-0 left-0 " >
+            Voltar
+        </div>
+    </Router-link>
     <div class="min-h-screen w-full flex flex-col gap-12 pb-12">
         <div class="mt-12 text-center text-white">
-        <h1 class="text-3xl">Sobre nós</h1>
-        <p class="text-xl">Uma breve apresentação sobre os desenvolvedores da DuckAI!</p>
+        <h1 class="text-5xl">Sobre nós</h1>
+        <p class="text-2xl">Uma breve apresentação sobre os desenvolvedores da DuckAI!</p>
+        <img src="/images/logo-pato.png" class="mx-auto w-20">
         </div>
         <div class="w-3/4 mx-auto flex gap-6" v-for="(dev, index) in devsData"  :key="dev.name" 
         :class="index % 2 === 0 ? 'flex-row-reverse' : 'flex-row'">
             <div class="w-4/12 h-1/2 ">
             <img :src="dev.url" class="w-full rounded-sm justify-self-end">
             </div>
-            <div class="flex flex-col">
-            <TitleSpecial :text="dev.nome" class="mt-12"/>
+            <div class="flex flex-col">    
+                <TitleSpecial :text="dev.nome" class="mt-12"/> 
             <p class="text-white text-xl text-left">{{ dev.description }}</p>
             </div>
-            <CirclesComponent/>
 
         </div>
     </div>
