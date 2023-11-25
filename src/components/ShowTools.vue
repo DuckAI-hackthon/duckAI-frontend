c33a4721ad664f79c2a66dc2664a6a31edfb5281
mateus-lopes <mateusalbano22@gmail.com>
2023-11-24 22:07:08 -0300

    chore - adicionando mudanca de ferramenta

--- src/components/ShowTools.vue
+++ src/components/ShowTools.vue
@@ -4,11 +4,15 @@
             title="Tradução de texto"
             description="With AI-powered image generation, designers and creatives can streamline their workflows and unlock new levels of efficiency."
             img="traducao-de-texto.png"
+            slug="TraductionToolComponent"
+            @changeTool="this.$emit('changeContentTo', 'TraductionToolComponent');"
         />
         <InfoTools
             title="Geração de resumo"
             description="With AI-powered image generation, designers and creatives can streamline their workflows and unlock new levels of efficiency."
             img="geracao-de-resumo.png"
+            slug="GenerationTextToolComponent"
+            @changeTool="this.$emit('changeContentTo', 'GenerationTextToolComponent');"
         />
     </div>
     <div class="flex gap-4">
@@ -16,11 +20,15 @@
             title="Palavras Chave"
             description="With AI-powered image generation, designers and creatives can streamline their workflows and unlock new levels of efficiency."
             img="palavras-chave.png"
+            slug="GenerationKeyWordToolComponent"
+            @changeTool="this.$emit('changeContentTo', 'GenerationKeyWordToolComponent');"
         />
         <InfoTools
             title="Perguntas e respostas"
             description="With AI-powered image generation, designers and creatives can streamline their workflows and unlock new levels of efficiency."
             img="perguntas-respostas.png"
+            slug="QuestionAndAnswerToolComponent"
+            @changeTool="this.$emit('changeContentTo', 'QuestionAndAnswerToolComponent');"
         />
     </div>
 </template>
@@ -29,7 +37,7 @@ import InfoTools from '@/components/InfoTool.vue'
 export default {
     components: {
         InfoTools
-    }
+    },
 }
 </script>
 <style>
