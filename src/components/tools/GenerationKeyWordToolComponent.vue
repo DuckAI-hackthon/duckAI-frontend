1f097dc32de6dc0c65a9b510fc46cc665b625721
luiznunes06 <luiz.nunes180106@gmail.com>
2023-11-24 23:56:59 -0300

    chore - continuação da ferramenta de geração de palavras chave

--- src/components/tools/GenerationKeyWordToolComponent.vue
+++ src/components/tools/GenerationKeyWordToolComponent.vue
@@ -1,45 +1,39 @@
 <script>
-import TopNav from "@/components/navbar-dashboard/TopNav.vue";
-import SideNav from "@/components/navbar-dashboard/SideNav.vue";
 import ToolTitleComponent from "@/components/tools/ToolTitleComponent.vue";
 
 export default {
   name: "DashBoard",
   components: {
-    TopNav,
-    SideNav,
     ToolTitleComponent,
   },
 };
 </script>
 <template>
-  <div class="w-full flex justify-center items-center bg-black text-white">
-    <header>
-      <TopNav />
-      <SideNav />
-    </header>
-    <main class="mt-32 w-8/12 m-auto">
-      <ToolTitleComponent text="Geração de palavras-chave " class="title" />
-      <div class="content">
-          <div class="text-box">
-              <p class="text">Texto</p>
-              <div class="contWord">10</div>
-            </div>
-            <div class="keyWords">
-                <p class="text">oi</p>
-            </div>
-        </div>
-    </main>
-    <footer></footer>
-  </div>
+  <article>
+    <ToolTitleComponent text="Geração de palavras-chave " />
+    <div class="flex-center gap-10">
+      <div class="text-box">
+        <textarea class="outline-none focus:border-none resize-none bg-transparent text-sm	 text-white p-2 ml-2 mt-1 w-[23vw] h-[28vh]"
+          name="" id=""></textarea>
+        <input class=" p-0 text-sm mt-6 w-10 h-10" type="number">
+      </div>
+      <div class="keyWords">
+        <textarea class="resize-none bg-transparent text-sm	 text-white p-2 ml-2 mt-1 w-[23vw] h-[28vh]" name="" id=""></textarea>
+      </div>
+    </div>
+  </article>
 </template>
 <style>
-.content{
-    display: flex;
-    justify-content: center;
-    gap: 2.5vw;
-    margin-top: 15vh;
+input{
+    @apply py-3 px-4 rounded-[10px] ;
+    border: 1px solid #6843EC;
+    background: #19161C;
+    box-shadow: 0px 58px 80px 0px rgba(0, 0, 0, 0.16);
+    resize: none;
+
 }
+
+
 .title {
   display: flex;
   justify-content: center;
@@ -49,17 +43,16 @@ export default {
   font-weight: 400;
   line-height: normal;
   letter-spacing: 0.96px;
-  background: linear-gradient(
-    90deg,
-    #ff3bff 0%,
-    #ecbfbf 38.02%,
-    #5c24ff 75.83%,
-    #d94fd5 100%
-  );
+  background: linear-gradient(90deg,
+      #ff3bff 0%,
+      #ecbfbf 38.02%,
+      #5c24ff 75.83%,
+      #d94fd5 100%);
   background-clip: text;
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
 }
+
 .text-box {
   width: 29vw;
   height: 29vh;
@@ -77,11 +70,13 @@ export default {
   line-height: normal;
   letter-spacing: 0.64px;
 }
+
 .text {
   font-size: 2rem;
   color: #8b8d90;
   margin: 3vh 0 0 2vw;
 }
+
 .contWord {
   color: #8b8d90;
   font-family: Poppins;
@@ -101,6 +96,7 @@ export default {
   justify-content: center;
   margin: 3vh 2vw 0 auto;
 }
+
 .keyWords {
   width: 38vw;
   height: 29vh;
