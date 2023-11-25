61ee5e9651e83f69fd57d640020cd69f047d55a1
RafaelBochi <rafaelbochibarros@gmail.com>
2023-11-25 04:33:13 -0300

    Update postTranslate function

--- src/components/tools/tradution-generator/ToolContainerComponent.vue
+++ src/components/tools/tradution-generator/ToolContainerComponent.vue
@@ -10,12 +10,15 @@ import { useUserStore } from "@/stores/user";
 const otherStore = useOtherStore();
 const userStore = useUserStore();
 
-const texto = ref("a");
-const resposta = ref("");
+const text = ref("a");
+const choice = ref("");
+const from_lang = ref("pt");
+const to_lang = ref("en");
 
 async function postTranslate() {
-  const data = await otherStore.postTranslate(texto.value, 'pt', 'en', userStore.userData.id, 1);
-    resposta.value = data.response.response;
+    console.log(from_lang.value, to_lang.value)
+    const data = await otherStore.postTranslate(text.value, from_lang.value, to_lang.value, userStore.userData.id, 1);
+    choice.value = data.response.response;
 }
 
 </script>
@@ -24,10 +27,10 @@ async function postTranslate() {
     <section class="flex flex-row justify-between mt-[10vh] pb-[19vh]">
         <div class="flex-col-center">
             <div class="flex gap-3 flex-row">
-                <select class="font-light gradient-border text-white w-[20vw]" name="" id="">
-                    <option value="">Português</option>
-                    <option value="">Inglês</option>
-                    <option value="">Alemão</option>
+                <select class="font-light gradient-border text-white w-[20vw]" name="" id="" v-model="from_lang">
+                    <option value="pt">Português</option>
+                    <option value="en">Inglês</option>
+                    <option value="de">Alemão</option>
                 </select>
                 <div class=" gradient-borderflex justify-center gradient-border">
                     <PdfSvgComponent />
@@ -42,10 +45,10 @@ async function postTranslate() {
 
         <div class="flex-col-center">
             <div class="flex gap-3 flex-row">
-                <select class="font-light gradient-border text-white w-[24vw]" name="" id="">
-                    <option value="">Português</option>
-                    <option value="">Inglês</option>
-                    <option value="">Alemão</option>
+                <select class="font-light gradient-border text-white w-[24vw]" name="" id="" v-model="to_lang">
+                    <option value="pt">Português</option>
+                    <option value="en">Inglês</option>
+                    <option value="de">Alemão</option>
                 </select>
             </div>
             <textarea class="gradient-border text-white  mt-10 w-[24vw] h-[30vh]" name="" id=""
